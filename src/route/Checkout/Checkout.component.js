ea22fc1abefba13bfd9ecf542c77ac38391e49d1
Erikyy <erikyl@hotmail.com>
2021-12-22 15:35:38 +0200

    Add override for checkout component

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -1,8 +1,34 @@
-import { Checkout as SourceCheckout } from 'Route/Checkout/Checkout.component';
-
+import { Checkout as SourceCheckout } from 'SourceRoute/Checkout/Checkout.component'
+import ContentWrapper from 'SourceComponent/ContentWrapper';
 
 export class Checkout extends SourceCheckout {
-
+    renderProgressbar() {
+        return <h1>progress bar</h1>
+    }
+    
+    render() {
+        return (
+            <main block="Checkout">
+                { this.renderProgressbar() }
+                <ContentWrapper
+                  wrapperMix={ { block: 'Checkout', elem: 'Wrapper' } }
+                  label={ __('Checkout page') }
+                >
+                    { this.renderSummary(true) }
+                    <div block="Checkout" elem="Step">
+                        { this.renderTitle() }
+                        { this.renderGuestForm() }
+                        { this.renderStep() }
+                        { this.renderLoader() }
+                    </div>
+                    <div>
+                        { this.renderSummary() }
+                        { this.renderPromo() }
+                        { this.renderCoupon() }
+                    </div>
+                </ContentWrapper>
+            </main>
+        );
+    }
 }
-
 export default Checkout;
